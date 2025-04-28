c311574e9438a9b6990b315f983f9101cc991398
thepsySunny <tjsgnl2002@thepsyentist.com>
2025-04-28 22:57:44 +0900

    refactor: contenthash적용

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
   },
   module: {
     rules: [
