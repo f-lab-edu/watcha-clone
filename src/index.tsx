de058a7c4a0b8932ba1ab9bf44dd3df68b54864e
thepsySunny <tjsgnl2002@thepsyentist.com>
2025-05-20 22:59:38 +0900

    feat: react-query-devtools 추가

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "@Components/ErrorFallback";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <App />
         </Suspense>
       </ErrorBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
