83029cd53f7ce86ae9695a20e3ac627a54fcd79c
thepsySunny <tjsgnl2002@thepsyentist.com>
2025-05-20 23:39:52 +0900

    refactor: webpack 코드스플리팅 관련 옵션 추가

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,7 +24,25 @@ module.exports = merge(commonConfig, {
   optimization: {
     minimize: true,
     splitChunks: {
-      chunks: 'all',
-    }
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
   }
 });
