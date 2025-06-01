acbba3d85c64ef13e7964b224c99ea008f6f0a84
thepsySunny <tjsgnl2002@thepsyentist.com>
2025-06-02 00:04:03 +0900

    feat: 기본 디테일 페이지 콘텐츠 배치

--- src/pages/DetailMovie.tsx
+++ src/pages/DetailMovie.tsx
@@ -1,5 +1,7 @@
 import { useFetchDetailMovie } from "@Apis/fetchDetailMovie";
+import { TopHeader } from "@Components/TopHeader";
 import { useParams } from "react-router-dom";
+import { getRunningTime } from "../utils/getRunningTime";
 
 const DetailMovie = () => {
   const { id } = useParams();
@@ -9,8 +11,63 @@ const DetailMovie = () => {
 
   return (
     <div>
-      <h1 style={{ color: "red", padding: "20px" }}>Detail Movie Page</h1>
-      {data.overview}
+      <TopHeader />
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          gap: "16px",
+          padding: "16px",
+          height: "fit-content",
+        }}
+      >
+        <div>
+          <h1>{data.title}</h1>
+          <div>
+            <span>12세</span>·<span>평균 {data.vote_average}</span>·
+            <span>{data.release_date.slice(0, 4)}</span>·
+            <span>{getRunningTime(data.runtime)}</span>·
+            {data.genres.map((genre, index, array) => (
+              <span key={genre.id} style={{ marginRight: "8px" }}>
+                {genre.name}
+                {index < array.length - 1 ? " · " : ""}
+              </span>
+            ))}
+          </div>
+          <p
+            style={{
+              maxWidth: "440px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: 3,
+              WebkitBoxOrient: "vertical",
+              marginBottom: "16px",
+            }}
+          >
+            {data.overview}
+          </p>
+        </div>
+        <div
+          style={{
+            width: "500px",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            aspectRatio: "16/9",
+          }}
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+            alt={data.title}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -2,6 +2,7 @@ export type Movie = {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
+  genres: Genre[];
   id: number;
   original_language: string;
   original_title: string;
@@ -13,4 +14,10 @@ export type Movie = {
   video: boolean;
   vote_average: number;
   vote_count: number;
+  runtime: number;
+};
+
+type Genre = {
+  id: number;
+  name: string;
 };
